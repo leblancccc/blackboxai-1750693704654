10bee1f7b04c30b21daee2755418e72899aeb052
leblancccc <137217587+leblancccc@users.noreply.github.com>
2025-06-24 00:18:33 +0800

    Add Taxi-Booking-System/Taxi-Booking-System-master/dbms/driver_management.py 1750695514141

--- Taxi-Booking-System/Taxi-Booking-System-master/dbms/driver_management.py
+++ Taxi-Booking-System/Taxi-Booking-System-master/dbms/driver_management.py
@@ -1,13 +1,12 @@
 from libs.driver_libs import Driver_Libs
 from dbms.connection import Connect
-import mysql.connector
 import sys
 
 def insert_record(driverInfo):
     conn=None
-    sql="""INSERT INTO `drivers` VALUES (?,?,?,?,?,?,?,?,?)"""
+    sql="""INSERT INTO drivers VALUES (?,?,?,?,?,?,?,?)"""
     values=(driverInfo.getDid(), driverInfo.getName(), driverInfo.getMobile(), driverInfo.getEmail(),
-            driverInfo.getLicense(), driverInfo.getPassword(),driverInfo.getDriverstatus(), driverInfo.getStatus(), driverInfo.getVehicletype())
+            driverInfo.getLicense(), driverInfo.getPassword(), driverInfo.getDriverstatus(), driverInfo.getVehicletype())
     result=False
     try:
         conn=Connect()
@@ -18,7 +17,6 @@ def insert_record(driverInfo):
         conn.close()
         result=True
 
-
     except:
         print("Error", sys.exc_info())
 
@@ -26,11 +24,10 @@ def insert_record(driverInfo):
         del sql
         del conn
         return result
-        return values
 
 def search_record(did):
     conn=None
-    sql="""SELECT *, old_password(password) as Pw FROM drivers WHERE did=?"""
+    sql="""SELECT * FROM drivers WHERE did=?"""
     values=(did,)
     searchResult=None
     try:
@@ -65,14 +62,14 @@ def delete_record(did):
         print("Error", sys.exc_info())
 
     finally:
-
         del values, sql,conn
         return result
 
 def update_record(driverInfo):
     conn=None
     sql="""UPDATE drivers SET name=?, mobile=?, email=?, license=?, vehicletype=? WHERE did=?"""
-    values=(driverInfo.getName(), driverInfo.getMobile(), driverInfo.getEmail(), driverInfo.getLicense(), driverInfo.getVehicletype(), driverInfo.getDid())
+    values=(driverInfo.getName(), driverInfo.getMobile(), driverInfo.getEmail(), 
+            driverInfo.getLicense(), driverInfo.getVehicletype(), driverInfo.getDid())
     updateresult=False
     try:
         conn=Connect()
@@ -113,7 +110,7 @@ def update_DriverStatus(driverInfo):
 
 def driver_select_all(driverID):
     conn=None
-    sql="""SELECT * FROM drivers WHERE did=%s"""
+    sql="""SELECT * FROM drivers WHERE did=?"""
     values=(driverID,)
     selectResult=None
     try:
@@ -124,14 +121,12 @@ def driver_select_all(driverID):
         cursor.close()
         conn.close()
 
-
     except:
         print("Error", sys.exc_info())
 
     finally:
         del values, sql, conn
         return selectResult
-
 
 def total_driver():
     conn=None
@@ -192,9 +187,11 @@ def select_alldriver():
 
 def driver_selectallbooking(did):
     conn=None
-    sql="""select booking.bookingid, booking.pickupaddress, booking.date, booking.time, booking.dropoffaddress,
-     customers.name, booking.bookingstatus from booking inner join customers
-      on booking.cid=customers.cid where booking.did=%s and booking.bookingstatus='Booked'"""
+    sql="""SELECT booking.bookingid, booking.pickupaddress, booking.date, booking.time, booking.dropoffaddress,
+           customers.name, booking.bookingstatus 
+           FROM booking 
+           INNER JOIN customers ON booking.cid=customers.cid 
+           WHERE booking.did=? AND booking.bookingstatus='Booked'"""
     values=(did,)
     driverResult=None
     try:
@@ -214,17 +211,16 @@ def driver_selectallbooking(did):
 
 def driver_select_all22(name11):
     conn=None
-    sql="""SELECT * FROM drivers WHERE name LIKE '%{}%'""".format(name11)
-
+    sql="""SELECT * FROM drivers WHERE name LIKE ?"""
+    values=('%' + name11 + '%',)
     selectResult=None
     try:
         conn=Connect()
         cursor=conn.cursor()
-        cursor.execute(sql)
+        cursor.execute(sql, values)
         selectResult=cursor.fetchall()
         cursor.close()
         conn.close()
-
 
     except:
         print("Error", sys.exc_info())
@@ -235,9 +231,11 @@ def driver_select_all22(name11):
 
 def driver_trip_history(did):
     conn=None
-    sql="""select customers.cid, customers.name, booking.date, booking.time, 
-    booking.pickupaddress, booking.dropoffaddress from booking inner join customers
-     on booking.cid=customers.cid where booking.did=%s and booking.bookingstatus='Billing Completed'"""
+    sql="""SELECT customers.cid, customers.name, booking.date, booking.time,
+           booking.pickupaddress, booking.dropoffaddress 
+           FROM booking 
+           INNER JOIN customers ON booking.cid=customers.cid 
+           WHERE booking.did=? AND booking.bookingstatus='Billing Completed'"""
     values=(did,)
     driverResult=None
     try:
@@ -257,7 +255,7 @@ def driver_trip_history(did):
 
 def driver_password_change(Driver):
     conn=None
-    sql="""UPDATE drivers SET password=%s WHERE did=%s"""
+    sql="""UPDATE drivers SET password=? WHERE did=?"""
     values=(Driver.getPassword(), Driver.getDid())
     changepasswordresult=False
 
