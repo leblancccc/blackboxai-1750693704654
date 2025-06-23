f56a0dccc690e7ae219d5013f70d12c572e6fc48
leblancccc <137217587+leblancccc@users.noreply.github.com>
2025-06-23 23:48:48 +0800

    Add Taxi-Booking-System/Taxi-Booking-System-master/libs/driver_libs.py 1750693728419

---
+++ Taxi-Booking-System/Taxi-Booking-System-master/libs/driver_libs.py
@@ -0,0 +1,71 @@
+
+class Driver_Libs():
+    def __init__(self, did=0, name=None, mobile=None, email=None,license=None, password=None, status=None, driverstatus=None, vehicletype=None):
+        self.did=did
+        self.name=name
+        self.mobile=mobile
+        self.email=email
+        self.license=license
+        self.password=password
+        self.status=status
+        self.driverstatus=driverstatus
+        self.vehicletype=vehicletype
+
+    #getter
+    def getDid(self):
+        return self.did
+
+    def getName(self):
+        return self.name
+
+    def getMobile(self):
+        return self.mobile
+
+    def getEmail(self):
+        return self.email
+
+    def getLicense(self):
+        return self.license
+
+    def getPassword(self):
+        return self.password
+
+    def getStatus(self):
+        return self.status
+
+    def getDriverstatus(self):
+        return self.driverstatus
+
+    def getVehicletype(self):
+        return self.vehicletype
+
+    #setter
+    def setDid(self, did):
+        self.did=did
+
+    def setName(self, name):
+        self.name=name
+
+    def setMobile(self, mobile):
+        self.mobile=mobile
+
+    def setEmail(self, email):
+        self.email=email
+
+    def setLicense(self, license):
+        self.license=license
+
+    def setPassword(self, password):
+        self.password=password
+
+    def setStatus(self, status):
+        self.status=status
+
+    def setDriverstatus(self, driverstatus):
+        self.driverstatus=driverstatus
+
+    def setVehicletype(self, vehicletype):
+        self.vehicletype=vehicletype
+
+    def __str__(self):
+        return ('{},{},{},{},{},{},{},{},{}'.format(self.did, self.name, self.mobile, self.email, self.license, self.password, self.status, self.driverstatus, self.vehicletype))
